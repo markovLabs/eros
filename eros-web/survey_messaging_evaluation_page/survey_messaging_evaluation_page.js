f65d25fe5162e11dbf942210151697e6c1729141
Victor Regalado <vrrs0988@gmail.com>
2017-07-23 18:44:05 -0400

    ERS-003 Added controllers to all pages

---
+++ eros-web/survey_messaging_evaluation_page/survey_messaging_evaluation_page.js
@@ -0,0 +1,14 @@
+var app = angular.module("surveyMsgEvaluation", ['ngMaterial']); 
+app.controller("surveyMsgEvaluationController",function($scope, $http, $window){ 
+	$scope.onContinue=function(){
+		if(angular.isUndefined($scope.firstName) || angular.isUndefined($scope.lastName) || !$scope.agree){
+			$window.alert("Please write your first name, last name and agree to consent to continue.")
+		} else{
+	    	var daterURL = $scope.erosBaseUrl + "/daters/" + $window.sessionStorage.getItem('dater_id');
+	    	var dater = {name:$scope.firstName, last_name:$scope.lastName, accepted_consent:true};
+	    	$http.post(daterURL, dater).then(function(response){
+	    		$window.location.href="../acceptance_question/acceptance_question.html";
+	    	});
+		}
+	}
+})
