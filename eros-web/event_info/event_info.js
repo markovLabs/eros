5f340bace04147d4f201a3ae405eb84372b25140
Victor Regalado <vrrs0988@gmail.com>
2017-07-24 19:16:01 -0400

    ERS-003 Added event info

--- eros-web/event_info/event_info.js
+++ eros-web/event_info/event_info.js
@@ -1,14 +1,38 @@
+function getNextEvent($http, baseURL){
+	var event;
+	$http.get(baseURL + "/events?next_event=true").then(function(response){
+		var events = response.data.events
+		if(events.length != 0) {
+			event = events[0];
+		}
+	});
+	return event;
+}
+
+function setEvent($scope, $http){
+	return function(){
+		var event = getNextEvent($http, $scope.erosBaseUrl)
+		if(angular.isDefined(event)) {
+			$scope.event = event
+			if(event.started) {
+				$scope.disableContinueButton = false
+			} else{
+				$scope.disableContinueButton = true
+			}
+		} else {
+			$scope.disableContinueButton = true
+			$scope.event = {name:"Sorry no event scheduled. Please check again later", location:"", date:""}
+		}
+	}
+}
+
 var app = angular.module("eventInfo", ['ngMaterial']); 
-app.controller("eventInfoController",function($scope, $http, $window){ 
+app.controller("eventInfoController",function($scope, $http, $window, $interval){ 
+	setEvent($scope, $http).apply();
+	$interval(setEvent($scope, $http), 3000);
 	$scope.onContinue=function(){
-		if(angular.isUndefined($scope.firstName) || angular.isUndefined($scope.lastName) || !$scope.agree){
-			$window.alert("Please write your first name, last name and agree to consent to continue.")
-		} else{
-	    	var daterURL = $scope.erosBaseUrl + "/daters/" + $window.sessionStorage.getItem('dater_id');
-	    	var dater = {name:$scope.firstName, last_name:$scope.lastName, accepted_consent:true};
-	    	$http.post(daterURL, dater).then(function(response){
-	    		$window.location.href="../acceptance_question/acceptance_question.html";
-	    	});
+		if(!$scope.disableContinueButton){
+	    	$window.location.href="../profile_evaluation_page/profile_evaluation_page.html";
 		}
 	}
 })
