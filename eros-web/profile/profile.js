d105f0966371087113ca73f3a9d037451a5c281f
Victor Regalado <vrrs0988@gmail.com>
2017-07-26 00:09:18 -0400

    ERS-003 Fixed bug with line breaks on radio button based answers

--- eros-web/profile/profile.js
+++ eros-web/profile/profile.js
@@ -1,14 +1,33 @@
+function getNextEvent($http, baseURL){
+	var event;
+	$http.get(baseURL + "/events?next_event=true").then(function(response){
+		var events = response.data.events
+		if(events.length != 0) {
+			event = events[0];
+		}
+	});
+	return event;
+}
+
+function setEvent($scope, $http, $window){
+	return function(){
+		var event = getNextEvent($http, $scope.erosBaseUrl)
+		if(angular.isDefined(event)) {
+			$window.sessionStorage.setItem("event_id", event.getId());
+			$scope.disableContinueButton = false
+		} else {
+			$scope.disableContinueButton = true
+		}
+	}
+}
+
 var app = angular.module("profile", ['ngMaterial']); 
-app.controller("profileController",function($scope, $http, $window){ 
+app.controller("profileController",function($scope, $http, $window, $interval){
+	setEvent($scope, $http, $window).apply();
+	$interval(setEvent($scope, $http, $window), 3000);
 	$scope.onContinue=function(){
-		if(angular.isUndefined($scope.firstName) || angular.isUndefined($scope.lastName) || !$scope.agree){
-			$window.alert("Please write your first name, last name and agree to consent to continue.")
-		} else{
-	    	var daterURL = $scope.erosBaseUrl + "/daters/" + $window.sessionStorage.getItem('dater_id');
-	    	var dater = {name:$scope.firstName, last_name:$scope.lastName, accepted_consent:true};
-	    	$http.post(daterURL, dater).then(function(response){
-	    		$window.location.href="../acceptance_question/acceptance_question.html";
-	    	});
+		if(!$scope.disableContinueButton){
+	    	$window.location.href="../event_info/event_info.html";
 		}
 	}
 })
