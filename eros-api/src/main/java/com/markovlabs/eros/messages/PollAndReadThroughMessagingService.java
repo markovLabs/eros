60707e9d331f95f626301290952cccfa59f039c4
Victor Regalado <vrrs0988@gmail.com>
2017-08-10 16:01:56 -0400

    ERS-005 Completed UI first version

--- eros-api/src/main/java/com/markovlabs/eros/messages/PollAndReadThroughMessagingService.java
+++ eros-api/src/main/java/com/markovlabs/eros/messages/PollAndReadThroughMessagingService.java
@@ -4,7 +4,10 @@ import static com.markovlabs.eros.JOOQRecordUtility.addRecord;
 import static com.markovlabs.eros.model.tables.Messages.MESSAGES;
 import java.util.List;
 import static java.util.stream.Collectors.toList;
+
+import org.jooq.Condition;
 import org.jooq.DSLContext;
+import org.jooq.impl.DSL;
 
 public final class PollAndReadThroughMessagingService implements MessageService {
 	
@@ -23,13 +26,32 @@ public final class PollAndReadThroughMessagingService implements MessageService 
 	public List<Message> getMessages(MessageTO msgTO) {
 		long from = msgTO.getMsg().getFromDaterId();
 		long to = msgTO.getMsg().getToDaterId();
-		List<Message> allMsgs = erosDb.selectFrom(MESSAGES)
-				.where(MESSAGES.FROM_DATER_ID.eq(from)
-						.and(MESSAGES.TO_DATER_ID.eq(to)))
-				.orderBy(MESSAGES.TIME_RECEIVED)
-				.fetch()
-				.map(Message::of);
+		Condition condition = MESSAGES.FROM_DATER_ID.eq(from).and(MESSAGES.TO_DATER_ID.eq(to));
+		return getMessagesWhen(msgTO, condition);
+	}
+
+	@Override
+	public List<Message> getMessagesBetweenDaters(MessageTO msgTO) {
+		long from = msgTO.getMsg().getFromDaterId();
+		long to = msgTO.getMsg().getToDaterId();
+		Condition condition = MESSAGES.FROM_DATER_ID.eq(from).and(
+				(MESSAGES.TO_DATER_ID.eq(to)).or(MESSAGES.FROM_DATER_ID.eq(to).and(MESSAGES.TO_DATER_ID.eq(from))));
+		return getMessagesWhen(msgTO, condition);
+	}
+
+	private List<Message> getMessagesWhen(MessageTO msgTO, Condition condition) {
+		Condition eventCondition = getEventcondition(msgTO.getMsg().getEventId());
+		List<Message> allMsgs = erosDb.selectFrom(MESSAGES).where(condition.and(eventCondition))
+				.orderBy(MESSAGES.TIME_RECEIVED).fetch().map(Message::of);
 		int numOfUnsentMsgs = allMsgs.size() - msgTO.getMessageReceived();
 		return allMsgs.stream().limit(numOfUnsentMsgs).collect(toList());
+	}
+
+	private Condition getEventcondition(Long eventId) {
+		if (eventId != null) {
+			return MESSAGES.EVENT_ID.equal(eventId);
+		} else {
+			return DSL.trueCondition();
+		}
 	}
 }
--- eros-web/messaging_evaluation_page/messaging_evaluation_page.js
+++ eros-web/messaging_evaluation_page/messaging_evaluation_page.js
@@ -1,3 +1,13 @@
+function setMsgProfileImages($scope, $http){
+	$scope.profileImgs = new Map()
+	$http.get($scope.erosBaseUrl + "/daters/" + $scope.matchId + "/images/").then(function(response){
+		$scope.profileImgs.set($scope.matchId, "http://69.164.208.35:8001/imgs/" + response.data.images[i].name);
+	});
+	$http.get($scope.erosBaseUrl + "/daters/" + $scope.daterId + "/images/").then(function(response){
+		$scope.profileImgs.set($scope.daterId, "http://69.164.208.35:8001/imgs/" + response.data.images[i].name);
+	});
+}
+
 function setProfileImages($scope, $http){
 	$http.get($scope.erosBaseUrl + "/daters/" + $scope.matchId + "/images/").then(function(response){
 		var images = response.data.images;
@@ -5,7 +15,7 @@ function setProfileImages($scope, $http){
 			imgSrc = "http://69.164.208.35:8001/imgs/" + images[i].name;
 			$scope.slides.push({src : imgSrc, id:i});
 		}
-		$scope.profileImage = $scope.slides[0].src;
+		$scope.profileImage = 	$scope.slides[0].src;
 	});
 }
 
@@ -15,6 +25,7 @@ function setProfile(matches, $window, $scope, $http){
 	$scope.matchId = matchAndStoryId.match.id;
 	$scope.matchName = matchAndStoryId.match.profile_name;
 	setProfileImages($scope, $http);
+	setMsgProfileImages($scope, $http);
 	$scope.buttonLabel = "Take survey";
 }
 
@@ -55,7 +66,7 @@ function getStoryLabel(answerType, daterAnswer, matchAnswer){
 }
 
 var app = angular.module("msgEvaluation", ['ngMaterial', 'ngAnimate', 'ngSanitize', 'ui.bootstrap']); 
-app.controller("msgEvaluationController",function($scope, $http, $window, $q){ 
+app.controller("msgEvaluationController",function($scope, $http, $window, $q, $interval, $mdToast){ 
 	$scope.disableContinueButton = true;
 	$scope.erosBaseUrl = 'http://69.164.208.35:17320/eros/v1'
 	$scope.eventId = $window.sessionStorage.getItem("event_id");
@@ -72,6 +83,18 @@ app.controller("msgEvaluationController",function($scope, $http, $window, $q){
 		setProfile(matches, $window, $scope, $http);
 	}
 	
+	$interval(function(){
+		var url = $scope.erosBaseUrl + "/messages/?from=" + $scope.matchId + "&to=" + $scope.daterId + "&messages_received=" 
+			+ $scope.msgs.length + "&event_id=" + $scope.eventId;
+		$http.get(url)
+		.then(function(response){
+			var msgs = response.data.messages;
+			for (var i = 0; i < msgs.length; i++){
+				$scope.msgs.push(msgs[i].msg)
+			}
+		});
+	}, 100);
+	
 	$timeout(function(){
 		$scope.disableContinueButton = false
 	}, 180000);
@@ -80,5 +103,14 @@ app.controller("msgEvaluationController",function($scope, $http, $window, $q){
 		if(!$scope.disableContinueButton){
 			$window.location.href="survey_messaging_evaluation_page.html";
 		}
+	}
+	
+	$scope.onSendMsg=function(){
+		var url = $scope.erosBaseUrl + "/messages/";
+		var msg = {from_dater_id:$scope.daterId, to_dater_id:$scope.matchId, event_id:$scope.eventId, content:$scope.msgSent}
+		$http.post(url, msg).then(angular.noop(), function(err){
+			$mdToast.simple().textContent('Message was not sent. Please try again.').hideDelay(3000)
+			console.log(err.data)
+		});
 	}
 })
